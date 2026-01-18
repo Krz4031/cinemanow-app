1900afb2232a331f65ab0327da8afcf56618a5c5
kawinnat-kran <kawinnat.dev01@gmail.com>
2026-01-18 23:33:48 +0700

    fix base path for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/cinemanow-app/",
+  // ❌ เอา base ของ GitHub Pages ออก
+  // base: "/cinemanow-app/",
 });
